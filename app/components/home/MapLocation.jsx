e6432201d450d2b082231bf77b0708a764a107cc
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-10-21 11:25:55 +0500

    Window error fixed

--- app/components/home/MapLocation.jsx
+++ app/components/home/MapLocation.jsx
@@ -1,17 +1,14 @@
-"use client"
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+"use client";
+import React, { useEffect } from 'react';
+import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 import { motion } from 'framer-motion';
 
-// Fix for default marker icon issue in Leaflet
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-});
+// Dynamically import MapContainer, TileLayer, Marker, and Popup from react-leaflet
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
 const MapLocation = () => {
   const markers = [
@@ -19,6 +16,20 @@ const MapLocation = () => {
     { position: [51.515, -0.1], popup: "Marker 2" },
     { position: [51.525, -0.11], popup: "Marker 3" },
   ];
+
+  useEffect(() => {
+    // Ensure this code runs only on the client side
+    if (typeof window !== "undefined") {
+      // Fix for default marker icon issue in Leaflet
+      const L = require('leaflet');
+      delete L.Icon.Default.prototype._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+        iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+      });
+    }
+  }, []);
 
   return (
     <motion.div
--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import LeftHeader from "./components/common/SideBar";
